f562b396c4c93bf80ac987980708abd2ef8d54b1
NmlessTAK <Nmless@gmail.com>
2023-12-18 08:27:53 +0100

    database

---
+++ strona.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2023 at 08:22 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `strona`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `komentarze`
+--
+
+CREATE TABLE `komentarze` (
+  `id_komentarza` int(11) NOT NULL,
+  `id_uzytkownika` text DEFAULT NULL,
+  `komentarz` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `logowanie`
+--
+
+CREATE TABLE `logowanie` (
+  `id_uzytkownika` int(11) NOT NULL,
+  `Nazwa_uzytkownika` text DEFAULT NULL,
+  `haslo` text DEFAULT NULL,
+  `email` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `komentarze`
+--
+ALTER TABLE `komentarze`
+  ADD PRIMARY KEY (`id_komentarza`);
+
+--
+-- Indeksy dla tabeli `logowanie`
+--
+ALTER TABLE `logowanie`
+  ADD PRIMARY KEY (`id_uzytkownika`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `komentarze`
+--
+ALTER TABLE `komentarze`
+  MODIFY `id_komentarza` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `logowanie`
+--
+ALTER TABLE `logowanie`
+  MODIFY `id_uzytkownika` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
